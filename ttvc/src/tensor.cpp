015707684d00da3cd2077c4d6d76d6d26a52fe9f
Zeyu Li <li_zeyu@pku.edu.cn>
2023-01-31 13:23:42 +0800

    add vint constructor

--- ttvc/src/tensor.cpp
+++ ttvc/src/tensor.cpp
@@ -4,6 +4,16 @@ Tensor::Tensor() {
     data = nullptr;
 }
 
+Tensor::Tensor(vint shape_in) {
+    shape = shape_in;
+    size = 1;
+    ndim = shape.size();
+    for (int ii = 0; ii < ndim; ii++) 
+        size *= shape[ii];
+    data = reinterpret_cast<double *>(malloc(sizeof(double) * size));
+    std::memset(data, 0, sizeof(double) * size);
+}
+
 void Tensor::constructor(vint shape_in) {
     shape = shape_in;
     size = 1;
