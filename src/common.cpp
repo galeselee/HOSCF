c5d17ff37d10aaa4f32df4dd0946e1e4445d7baf
zeyuli <li_zeyu@pku.edu.cn>
2023-02-12 14:23:56 +0800

    update readme

--- src/common.cpp
+++ src/common.cpp
@@ -14,11 +14,13 @@ void timettvc(std::function<void(Tensor *, Tensor *, Tensor *, int, int)> f,
          << "ms" << std::endl;
 }
 
+extern int rank;
 void timescf(std::function<void(Tensor *, Tensor *, double, uint32_t)> f,
              Tensor *A, Tensor *U, double tol, uint32_t max_iter) {
     auto start = std::chrono::system_clock::now();
     f(A, U, tol, max_iter);
     auto end = std::chrono::system_clock::now();
+	if (rank==1)
     std::cout << "time : " \
          << std::chrono::duration_cast<std::chrono::microseconds>(end-start).count() / 1000.0
          << "ms" << std::endl; 
